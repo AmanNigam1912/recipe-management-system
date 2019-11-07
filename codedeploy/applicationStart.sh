a85bf87e86afe1e6cf475e32c27635f70baf0743
Aman Nigam <nigam.ama@husky.neu.edu>
2019-11-06 19:24:30 -0500

    updated cloud watch agent in application start

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -5,13 +5,15 @@
 #     -m ec2 \
 #     -c file:/opt
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl 
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/aws/amazon-cloudwatch-agent/cloudwatch-config.json \
-    -s
-sudo systemctl start amazon-cloudwatch-agent
-sudo systemctl enable amazon-cloudwatch-agent
+# sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl 
+#     -a fetch-config \
+#     -m ec2 \
+#     -c file:/opt/aws/amazon-cloudwatch-agent/cloudwatch-config.json \
+#     -s
+
 cd /usr/share/tomcat/bin
 ./startup.sh
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/cloudwatch-config.json -s
+sudo systemctl start amazon-cloudwatch-agent
 #sudo systemctl start tomcat.service
